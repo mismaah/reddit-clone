d1bd64bf5691029b3e0b733e87a1e5118bbca105
mismaah <mismaahabdulla@gmail.com>
2020-08-25 19:50:10 +0500

    add sub route

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,18 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Thread from '../views/Thread.vue'
+import Sub from '../views/Sub.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
     {
         path: '/',
+        name: 'Sub',
+        component: Sub
+    },
+    {
+        path: '/thread',
         name: 'Thread',
         component: Thread
     }
