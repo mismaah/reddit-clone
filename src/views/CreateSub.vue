f6a52bcbdaac3eb59f79a5bf7826615b374c28a2
mismaah <mismaahabdulla@gmail.com>
2020-08-26 18:47:22 +0500

    add stuff

---
+++ src/views/CreateSub.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="centered">
+        <input placeholder="Enter sub name" v-model="subName" type="text">
+        <button>Create sub</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "CreateSub",
+    data: () => ({
+        subName: "",
+    })
+}
+</script>
+
+<style scoped>
+.centered {
+    padding: 20px;
+    display: flex;
+    justify-content: center;
+}
+input {
+    margin-right: 10px;
+}
+</style>
