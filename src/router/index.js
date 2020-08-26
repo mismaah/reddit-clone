1c524a935c0d7830e63c6f432285f2d5147c1e5f
mismaah <mismaahabdulla@gmail.com>
2020-08-26 20:04:13 +0500

    create Create thread view"

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Thread from '../views/Thread.vue'
 import Sub from '../views/Sub.vue'
 import Home from '../views/Home.vue'
 import CreateSub from '../views/CreateSub.vue'
+import CreateThread from '../views/CreateThread.vue'
 
 Vue.use(VueRouter)
 
@@ -25,8 +26,13 @@ const routes = [
     },
     {
         path: '/createsub',
-        name: 'CreateSub',
+        name: 'Create Sub',
         component: CreateSub
+    },
+    {
+        path: '/createthread',
+        name: 'Create Thread',
+        component: CreateThread
     },
 ]
 
---
+++ src/views/CreateThread.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <span class="header">
+            <p class="title">r/{{subName}}</p>
+        </span>
+        <div class="divider"></div>
+        <div class="centered">
+            <input placeholder="Enter thread title" v-model="threadTitle" type="text">
+            <textarea v-model="threadBody" rows="3" placeholder="Enter thread body"></textarea>
+        </div>
+        <button>Create thread</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "CreateSub",
+    data: () => ({
+        subName: "something",
+        threadTitle: "",
+        threadBody: "",
+    })
+}
+</script>
+
+<style scoped>
+.header {
+    display: flex;
+    justify-content: space-between;
+}
+.title {
+    font-size: large;
+    font-weight: bold;
+    margin-bottom: 0px;
+    margin-left: 50px;
+}
+.centered {
+    display: flex;
+    flex-direction: column;
+    margin: auto;
+    width: 60%;
+}
+textarea, input {
+  font-family: inherit;
+  font-size: 15px;
+  margin-bottom: 10px;
+}
+</style>
