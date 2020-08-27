b7a13fe6c644f25e5c0a591530514250d266f21b
mismaah <mismaahabdulla@gmail.com>
2020-08-27 23:05:46 +0500

    add api calls, error handling etc

--- src/views/CreateSub.vue
+++ src/views/CreateSub.vue
@@ -1,7 +1,10 @@
 <template>
-    <div class="centered">
-        <input placeholder="Enter sub name" v-model="subName" type="text">
-        <button>Create sub</button>
+    <div>
+        <div class="centered">
+            <input placeholder="Enter sub name" v-model="subname" type="text">
+            <button @click="createSub()">Create sub</button>
+        </div>
+        <p class="error" v-if="error">{{error}}</p>
     </div>
 </template>
 
@@ -9,16 +12,51 @@
     export default {
         name: "CreateSub",
         data: () => ({
-            subName: "",
-        })
+            subname: "",
+            error: null
+        }),
+        methods: {
+            createSub() {
+                this.error = null
+                if (this.subname == "") return
+                let createSub = {
+                    Subname: this.subname,
+                    CreatedBy: this.$store.getters.getCurrentUser
+                }
+                fetch(`${process.env.VUE_APP_BASE_URL}/api/createsub`, {
+                    method: 'post',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(createSub)
+                })
+                    .then(resp => {
+                        if (resp.ok) {
+                            this.$router.push(`/r/${this.subname}`)
+                        } else {
+                            return resp.text()
+                        }
+                    })
+                    .then(result => {
+                        this.error = result
+                    })
+            }
+        }
     }
 </script>
 
 <style scoped>
 .centered {
     padding: 20px;
+    padding-bottom: 10px;
     display: flex;
     justify-content: center;
+}
+.error {
+    margin: 0px;
+    font-size: 12px;
+    color: red;
 }
 input {
     margin-right: 10px;
