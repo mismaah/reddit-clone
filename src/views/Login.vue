f2143efd62aed63e41f622714eca406a54cc73fa
mismaah <mismaahabdulla@gmail.com>
2020-08-27 10:49:26 +0500

    populate views

---
+++ src/views/Login.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="centered">
+        <input class="item" placeholder="Enter username" v-model="username" type="text">
+        <input class="item" placeholder="Enter password" v-model="password" type="password">
+        <button class="item">Login</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Login",
+    data: () => ({
+        username: "",
+        password: "",
+    })
+}
+</script>
+
+<style scoped>
+.centered {
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+.item {
+    margin: 5px;
+}
+input {
+    margin-right: 10px;
+}
+</style>
---
+++ src/views/Register.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="centered">
+        <input class="item" placeholder="Enter username" v-model="username" type="text">
+        <input class="item" placeholder="Enter password" v-model="password" type="password">
+        <input class="item" placeholder="Re-enter password" v-model="passwordRe" type="password">
+        <input class="item" placeholder="Enter email address" v-model="email" type="text">
+        <button class="item">Register</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Register",
+    data: () => ({
+        username: "",
+        password: "",
+        passwordRe: "",
+        email: "",
+    })
+}
+</script>
+
+<style scoped>
+.centered {
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+.item {
+    margin: 5px;
+}
+input {
+    margin-right: 10px;
+}
+</style>
